a4f1ae522f567293eb7944a2490be1b1c460f76f
Ishan Joshi <ishanjoshi225@gmail.com>
2024-11-25 07:58:58 +0545

    first commit

---
+++ Controllers/Model/Table_1.cs
@@ -0,0 +1,12 @@
+﻿namespace repo.ishan.Controllers.Model;
+
+public class Table_1
+{
+   
+    public string First_Name {  get; set; }
+    public string Last_Name { get; set; }
+    public string Address { get; set; }
+    public string Phone_Number { get; set; }
+    public string Email { get; set; }
+    public int Id { get; set; }
+}
